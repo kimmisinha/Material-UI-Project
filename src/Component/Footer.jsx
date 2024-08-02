59473cfb21c56ad436d0fadcf07368bd5aa2080a
kimmisinha <kimmi.sinha@newput.com>
2024-08-02 11:52:56 +0530

    Update Footer: Divider 95%, remove link decorations

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,34 +4,32 @@ import YoutubeSearchedForRoundedIcon from "@mui/icons-material/YoutubeSearchedFo
 import OfflineBoltRoundedIcon from "@mui/icons-material/OfflineBoltRounded";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import Divider from '@mui/material/Divider';
-
+import Divider from "@mui/material/Divider";
 
 const Footer = () => {
   return (
     <Container
-      maxWidth={false}
-      style={{ marginTop: "160px" }}
+      maxWidth={"xl"}
+      style={{ marginTop: "200px" }}
       sx={{
-        width: "100%",
         backgroundColor: "rgb(255,255,255)",
         padding: "0px",
       }}
     >
-      <Grid container spacing={10} sx={{ margin: "auto" }}>
+      <Grid container spacing={10}>
         <Grid item xs={12} sm={4} sx={{ padding: 10 }}>
           <Typography
             variant="h5"
             component="h1"
             gutterBottom
-            sx={{ fontWeight: 600 }}
+            sx={{ fontWeight: 600, padding: "10px 0px 0px 30px" }}
           >
             Material Tailwind
           </Typography>
           <Typography
             variant="body2"
             color="text.secondary"
-            sx={{ fontSize: 16 }}
+            sx={{ fontSize: 16, padding: "10px 0px 0px 30px" }}
           >
             Easy to use React components for
             <br />
@@ -56,11 +54,11 @@ const Footer = () => {
             </Link>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} >
           <Typography
             variant="h6"
             component="div"
-            sx={{ mb: 1, fontWeight: "bold", fontSize: 16 }}
+            sx={{ mb: 1, fontWeight: "bold", fontSize: 16, paddingLeft: "300px" }}
           >
             Useful Links
           </Typography>
@@ -68,7 +66,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" ,paddingLeft: "300px"}}
           >
             About Us
           </Link>
@@ -76,7 +74,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" ,paddingLeft: "300px"}}
           >
             Blog
           </Link>
@@ -84,7 +82,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" ,paddingLeft: "300px"}}
           >
             Github
           </Link>
@@ -92,7 +90,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none",paddingLeft: "300px" }}
           >
             Free Products
           </Link>
@@ -101,7 +99,7 @@ const Footer = () => {
           <Typography
             variant="h6"
             component="div"
-            sx={{ mb: 1, fontWeight: "bold", fontSize: 16 }}
+            sx={{ mb: 1, fontWeight: "bold", fontSize: 16 ,paddingLeft: "100px"}}
           >
             Other Resources
           </Typography>
@@ -109,7 +107,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none",paddingLeft: "100px" }}
           >
             MIT License
           </Link>
@@ -117,7 +115,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none",paddingLeft: "100px" }}
           >
             Contribute
           </Link>
@@ -125,7 +123,7 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" ,paddingLeft: "100px"}}
           >
             Change Log
           </Link>
@@ -133,14 +131,14 @@ const Footer = () => {
             variant="body2"
             color="text.secondary"
             display="block"
-            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none" }}
+            sx={{ mb: 0.5, cursor: "pointer", textDecoration: "none",paddingLeft: "100px" }}
           >
             Contact Us
           </Link>
         </Grid>
       </Grid>
       {/* 1 */}
-      <Divider sx={{ width: "89%", margin: "0 auto",}} />
+      <Divider sx={{ width: "95%" }} />
       {/* <Box sx={{ borderTop: 1, borderColor: "divider", mt: 4, mx:'auto', w:'75%' }} /> */}
       <Typography
         variant="body2"
